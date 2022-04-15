4ea56410ebb40b0455cdf57cbf577212a1d0b4b7
IlhamFaturachman <69384419+IlhamFaturachman@users.noreply.github.com>
2022-04-16 02:04:15 +0700

    fasheng

--- lib/pages/murid/list_murid.dart
+++ lib/pages/murid/list_murid.dart
@@ -19,48 +19,50 @@ class _ListMurid extends State<ListMurid> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: () {
-                Route route =
-                    MaterialPageRoute(builder: (context) => InputAbsenGuru());
-                Navigator.push(context, route);
-              },
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: size.width * 0.045,
-                      top: size.height * 0.05,
-                    ),
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 50,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: size.width * 0.04, top: size.height * 0.05),
-                    child: Container(
-                      child: Text(
-                        "Student List",
-                        style: TextStyle(
-                            fontSize: 33, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Route route =
+                      MaterialPageRoute(builder: (context) => InputAbsenGuru());
+                  Navigator.push(context, route);
+                },
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.045,
+                        top: size.height * 0.05,
+                      ),
+                      child: Icon(
+                        Icons.arrow_back,
+                        size: 50,
                       ),
                     ),
-                  )
-                ],
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 0.04, top: size.height * 0.05),
+                      child: Container(
+                        child: Text(
+                          "Student List",
+                          style: TextStyle(
+                              fontSize: 33, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: size.height * 0.05, left: size.width * 0.08),
-              child: IsiListMurid(),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                    top: size.height * 0.05, left: size.width * 0.08),
+                child: IsiListMurid(),
+              ),
+            ],
+          ),
         ),
       ),
     );
