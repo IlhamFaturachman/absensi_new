91f4dd645dbeb7a9a0f33014174557c600dd8876
IlhamFaturachman <69384419+IlhamFaturachman@users.noreply.github.com>
2022-04-16 01:45:11 +0700

    detail absen updated

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color mainColour = Color(0xFF2C7AD6);
 const Color secondaryColour = Color(0xFF53D824);
 const Color formColor = Color(0xFFC6D2DF);
+const Color redColor = Color(0xFFD82424);
